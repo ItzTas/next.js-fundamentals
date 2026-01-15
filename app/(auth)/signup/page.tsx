78ddb9f855805f6c5bff31a2c82e777a6e03284b
Tales <59647566+ItzTas@users.noreply.github.com>
2026-01-15 18:31:56 -0400

    chore: class sorting

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -46,12 +46,12 @@ export default function SignUpPage() {
   }, intialState)
 
   return (
-    <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-50 dark:bg-[#121212]">
+    <div className="flex flex-col justify-center py-12 min-h-screen bg-gray-50 sm:px-6 lg:px-8 dark:bg-[#121212]">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h1 className="text-center text-3xl font-extrabold text-gray-900 dark:text-white">
+        <h1 className="text-3xl font-extrabold text-center text-gray-900 dark:text-white">
           Mode
         </h1>
-        <h2 className="mt-2 text-center text-2xl font-bold text-gray-900 dark:text-white">
+        <h2 className="mt-2 text-2xl font-bold text-center text-gray-900 dark:text-white">
           Create a new account
         </h2>
       </div>
@@ -125,13 +125,13 @@ export default function SignUpPage() {
             </Button>
           </div>
         </Form>
-        <div className="bg-white dark:bg-[#1A1A1A] py-8 px-4 shadow sm:rounded-lg sm:px-10 border border-gray-100 dark:border-dark-border-subtle">
+        <div className="py-8 px-4 bg-white border border-gray-100 shadow sm:px-10 sm:rounded-lg dark:bg-[#1A1A1A] dark:border-dark-border-subtle">
           <div className="mt-6 text-center">
             <p className="text-sm text-gray-600 dark:text-gray-400">
               Already have an account?{' '}
               <Link
                 href="/signin"
-                className="font-medium text-gray-900 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
+                className="font-medium text-gray-900 dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-100"
               >
                 Sign in
               </Link>
